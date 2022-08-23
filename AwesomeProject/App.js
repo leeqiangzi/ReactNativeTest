19a3dc55e7c04b6774effb1f2aabc795b522e4f7
lizhiqiang17 <lizhiqiang17@58.com>
2022-08-23 19:05:50 +0800

    导航添加

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -14,19 +14,40 @@ import {
 	StyleSheet,
 	Text,
 	View,
+	Button
 } from "react-native";
+import { NavigationContainer, } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }) {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Home Screen</Text>
+			<Button title="Go to Details"
+				onPress={() => navigation.navigate('Details')}
+			/>
+		</View>
+	);
+}
+
+function DetailsScreen() {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Details Screen</Text>
+		</View>
+	);
+}
 
 const App: () => Node = () => {
 	return (
-		<SafeAreaView style={{ flex: 1 }}>
-			<StatusBar barStyle={"dark-content"} />
-			<View style={{
-				width: 100,
-				height: 100,
-				backgroundColor: 'red'
-			}}>
-			</View>
-		</SafeAreaView>
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Home">
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Details" component={DetailsScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 };
 
