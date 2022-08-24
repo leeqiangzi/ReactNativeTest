68f7aa32ca400cb0499acd92590b6ec47789d77e
lizhiqiang17 <lizhiqiang17@58.com>
2022-08-24 14:48:01 +0800

    BottomTab导航控制器框架搭建

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -9,44 +9,27 @@
 import React from "react";
 import type { Node } from "react";
 import {
-	SafeAreaView,
-	StatusBar,
 	StyleSheet,
 	Text,
 	View,
 	Button
 } from "react-native";
-import { NavigationContainer, } from '@react-navigation/native';
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from "./Page/HomeScreen";
+import SettingsScreen from "./Page/SettingsScreen";
+import TestScreen from "./Page/TestScreen";
 
-const Stack = createNativeStackNavigator();
-
-function HomeScreen({ navigation }) {
-	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-			<Text>Home Screen</Text>
-			<Button title="Go to Details"
-				onPress={() => navigation.navigate('Details')}
-			/>
-		</View>
-	);
-}
-
-function DetailsScreen() {
-	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-			<Text>Details Screen</Text>
-		</View>
-	);
-}
+const Tab = createBottomTabNavigator();
 
 const App: () => Node = () => {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator initialRouteName="Home">
-				<Stack.Screen name="Home" component={HomeScreen} />
-				<Stack.Screen name="Details" component={DetailsScreen} />
-			</Stack.Navigator>
+			<Tab.Navigator>
+				<Tab.Screen name="Home" component={HomeScreen} />
+				<Tab.Screen name="Settings" component={SettingsScreen} />
+				<Tab.Screen name="Test" component={TestScreen} />
+			</Tab.Navigator>
 		</NavigationContainer>
 	);
 };
---
+++ AwesomeProject/Page/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+    Text,
+    View,
+} from "react-native";
+
+const HomeScreen = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home!</Text>
+        </View>
+    )
+}
+export default HomeScreen;
+
+// export default function HomeScreen() {
+    // return (
+    //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    //         <Text>Home!</Text>
+    //     </View>
+    // )
+// }
